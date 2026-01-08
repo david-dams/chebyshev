fix: vectorized normalization

vanilla_dense.py
@@ -39,9 +39,8 @@ def split_data(features, targets):
 
     ft, tt = features[training_idxs], targets[training_idxs]
     fv, tv= features[validation_idxs], targets[validation_idxs]
-    targets_mean, targets_sd = jnp.mean(tt), jnp.std(tt)
-    features_mean, features_sd = jnp.mean(ft), jnp.std(ft)
-
+    targets_mean, targets_sd = jnp.mean(tt, axis = 0), jnp.std(tt, axis = 0)
+    features_mean, features_sd = jnp.mean(ft, axis = 0), jnp.std(ft, axis = 0)
     
     data = {
         "train" : [normalize(ft, features_mean, features_sd), normalize(tt, targets_mean, targets_sd)],