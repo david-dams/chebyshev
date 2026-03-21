ad46f6bc2836bd7c36663484ab2cb11a456028f9
David Dams <david.dams@tutanota.com>
2026-03-21 12:00:00 +0100

    fix: key error

--- train.py
+++ train.py
@@ -371,7 +371,7 @@ def plot_validation_loss(data, err, xlabel, ylabel, name):
     
     print(name, {a : jnp.mean(b) for a, b in err.items()})
     
-    plt.plot(data["radius"], err)
+    plt.plot(data["radii"], err)
     plt.savefig(name)
     plt.close()
 
